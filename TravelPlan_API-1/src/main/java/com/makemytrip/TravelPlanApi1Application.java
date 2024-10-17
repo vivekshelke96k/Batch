36fbff9913f617f8249b1a9cb185eabc88559546
VIVEK <VIVEK@192.168.1.66>
2024-10-17 16:56:20 +0530

    Batch

---
+++ TravelPlan_API-1/src/main/java/com/makemytrip/TravelPlanApi1Application.java
@@ -0,0 +1,13 @@
+package com.makemytrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPlanApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelPlanApi1Application.class, args);
+	}
+
+}
---
+++ TravelPlan_API-1/src/test/java/com/makemytrip/TravelPlanApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makemytrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPlanApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
